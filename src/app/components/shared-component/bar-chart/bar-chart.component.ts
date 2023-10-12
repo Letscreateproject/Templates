3e5eec9bcf2220179d1633af77aa4c2a90a54f99
mithun <mithun.raj@bourntec.com>
2023-10-12 16:04:26 +0530

    applicant details and dashboard 2

---
+++ src/app/components/shared-component/bar-chart/bar-chart.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss'],
+})
+export class BarChartComponent implements OnInit {
+  constructor() {}
+
+  public chart: any;
+  ngOnInit(): void {
+    this.createChart();
+  }
+  createChart() {
+    const data = {
+      labels: [
+        '2022-05-10',
+        '2022-05-11',
+        '2022-05-12',
+        '2022-05-13',
+        '2022-05-14',
+        '2022-05-15',
+        '2022-05-16',
+        '2022-05-17',
+      ],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    this.chart = new Chart('MyChart', {
+      type: 'bar', //this denotes tha type of chart
+
+      data: data,
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+}
---
+++ src/app/components/shared-component/pie-chart/pie-chart.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-pie-chart',
+  templateUrl: './pie-chart.component.html',
+  styleUrls: ['./pie-chart.component.scss'],
+})
+export class PieChartComponent implements OnInit {
+  constructor() {}
+
+  public chart: any;
+  ngOnInit(): void {
+    this.createChart();
+  }
+  createChart() {
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow'],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    };
+
+    this.chart = new Chart('MyChart3', {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: data,
+      options: {
+        aspectRatio: 1.5,
+      },
+    });
+  }
+}
